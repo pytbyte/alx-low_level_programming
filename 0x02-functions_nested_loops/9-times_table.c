97e15a2cd5d9238f4c044df4b999932dad2acc37
pytbyte <heretolearn1@gmail.com>
2023-02-21 23:10:01 +0300

    a function that prints multiples of 9 starting with 0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * 9-times_table - Prints the multiplacation of 0 to 9 by 9.
+ */
+void 9-times_table(void)
+{
+	int x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar('0');
+
+		for (y = 1; y <= 9; y++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			z = x * y;
+
+			if (z <= 9)
+				_putchar(' ');
+			else
+				_putchar((z / 10) + '0');
+
+			_putchar((z % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
