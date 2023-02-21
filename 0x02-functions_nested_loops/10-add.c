2870f32f3d6fac685ecf55adfcdc43eda9099472
pytbyte <heretolearn1@gmail.com>
2023-02-21 23:19:10 +0300

    a function that prints the sum of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds int1 to int2.
+ * @num1: int1.
+ * @num2: int2.
+ *
+ * Return: The the sum int and int2.
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
