d0f6dc0662654e4de5cab311049cb018eeccc3a3
pytbyte <heretolearn1@gmail.com>
2023-02-25 02:01:44 +0300

    multiply two numbers and return results

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* mul - multiplies 2 integers
+* @a: first integer
+* @b: second integer
+* Return: product
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
