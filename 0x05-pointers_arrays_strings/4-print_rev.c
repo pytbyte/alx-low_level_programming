b8dad845ad8c71de3b30f65c08e7fab046f34669
pytbyte <heretolearn1@gmail.com>
2023-02-27 20:40:19 +0300

    reverse characters in a  string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,19 +7,18 @@
 void print_rev(char *s)
 {
 	int i;
-        int n = 0;
-	
+	int n = 0;
+
 	while (*s != '\0')
 	{
 		n++;
-                s++;
+		s++;
 	}
 	n--;
 	for (i = n ; i >= 0; i--)
 	{
 		s--;
 		_putchar(*s);
-		
 	}
 	_putchar('\n');
 }
