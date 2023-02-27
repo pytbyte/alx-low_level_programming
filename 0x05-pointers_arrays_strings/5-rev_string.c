1820ccbb0aae6b598cb8303998b2f718861c1914
pytbyte <heretolearn1@gmail.com>
2023-02-27 20:16:42 +0300

    a function that reverses a string.

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev -> printing a string in reverse
+ * @s: the string to be printed in rev
+ */
+void rev_string(char *s)
+{
+        int i;
+        int n = 0;
+
+        while (*s != '\0')
+        {
+                n++;
+                s++;
+        }
+        n--;
+        for (i = n ; i >= 0; i--)
+        {
+                s--;
+                _putchar(*s);
+
+        }
+        _putchar('\n');
+}
