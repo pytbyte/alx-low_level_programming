22a93e64ea9e7447c9b42f448eabe0455ccf1170
pytbyte <heretolearn1@gmail.com>
2023-03-01 19:57:48 +0300

    concat n chars from src to dst

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,33 +1,26 @@
 #include "main.h"
 
 /**
-*_strncat -concatates strings using n bytes
-*@dest:char
-*@src:char
-*@n:int
-*Return:char concatated
-*/
+ * _strncat - concatenates two strings
+ *
+ * @src: The source of strings
+ * @dest: The destination of the string
+ * @n: The length of int
+ *
+ * Return: pointer to the resulting string dest
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-	char *s = dest;
-	int x;
-	int y;
-	y = 0;
+	int i, j;
 
-	while (*dest != '\0')
+	for (i = 0; dest[i] != '\0'; i++)
 	{
-		dest++;
+		continue;
 	}
-
-	while (*src != '\0')
+	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
-		for (x = 0; src[x] != '\0' && x < n; x++)
-		{
-			dest[y] = src[x];
-		}
-		dest++;
-		src++;
+		dest[i + j] = src[j];
 	}
-	*dest = '\0';
-	return (s);
+	dest[i + j] = '\0';
+	return (dest);
 }
