f401ec845d84b64aad8b35953be9b3abb9435af0
pytbyte <heretolearn1@gmail.com>
2023-03-02 06:15:29 +0300

    corrections

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * 5-more_numbers - a function that prints 10 times the numbers, from 0 to 14, followed by a new line.
- * 
+ * more_numbers - numbers,from 0 to 14, followed by a new line.
  * Return: none
- */
+*/
 
 void more_numbers(void)
 {
