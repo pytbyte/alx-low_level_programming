10988a144c2424e4bdcef5b0655a26bde211cf06
pytbyte <heretolearn1@gmail.com>
2023-03-02 06:07:04 +0300

    a function that prints a[2] = 98

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,46 +1,18 @@
-#include "main.h"
-
-/**
- * infinite_add - adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: buffer for result
- * @size_r: buffer size
- * Return: address of r or 0
+#include <stdio.h>
+ /*
+   *  program prints a[2] = 98, followed by a new line.
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int i, j, k, l, m, n;
 
-	for (i = 0; n1[i]; i++)
-		;
-	for (j = 0; n2[j]; j++)
-		;
-	if (i > size_r || j > size_r)
-		return (0);
-	m = 0;
-	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
-	{
-		n = m;
-		if (i >= 0)
-			n += n1[i] - '0';
-		if (j >= 0)
-			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
-		{
-			break;
-		}
-		m = n / 10;
-		r[k] = n % 10 + '0';
-	}
-	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
-		return (0);
-	for (k -= 1, l = 0; l < k; k--, l++)
-	{
-		m = r[k];
-		r[k] = r[l];
-		r[l] = m;
-	}
-	return (r);
+int main(void)
+{
+  int n;
+  int a[5];
+  int *p;
+
+  a[2] = 1024;
+  p = &n;
+  *(p + 5) = 98;
+  
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
