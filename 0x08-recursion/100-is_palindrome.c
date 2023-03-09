05dcebda50a1a2a5bd5efaf34fc19f7dc5f4927e
pytbyte <heretolearn1@gmail.com>
2023-03-09 04:11:09 +0300

    Advanced Recursion manipulations with c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ *
+ * Return: 1 if it is, 0 it's not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (checker(s, 0, x_recursion(s)));
+}
+
+/**
+ * x_recursion - a function that returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
+ */
+int x_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + x_recursion(s + 1));
+}
+
+/**
+ * checker - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
+ *
+ * Return: 1 if palindrome else 0.
+ */
+int checker(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (checker(s, i + 1, len - 1));
+}
