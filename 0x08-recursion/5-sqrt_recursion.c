b68e4dde89dbb6093880f9fc1872c37b77a72844
pytbyte <heretolearn1@gmail.com>
2023-03-09 04:05:40 +0300

    Recursion manipulations with c corrections

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,12 +3,12 @@
 int base_sqrt_recursion(int n, int i);
 
 /**
- * base_sqrt_recursion - a function that returns the square root of a number
+ * _sqrt_recursion - a function that returns the square root of a number
  * @n: number to  check sq root
  *
  * Return: square root obtained
  */
-int base_sqrt_recursion(int n)
+int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
@@ -16,17 +16,17 @@ int base_sqrt_recursion(int n)
 }
 
 /**
- * _sqrt_recursion - This recurses to find the square root of a number
+ * base_sqrt_recursion - This recurses to find the square root of a number
  * @n: number to calculate the sqaure root of
  * @i: iterator
  *
  * Return: the resulting square root
  */
-int _sqrt_recursion(int n, int i)
+int base_sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (_sqrt_recursion(n, i + 1));
+	return (base_sqrt_recursion(n, i + 1));
 }
