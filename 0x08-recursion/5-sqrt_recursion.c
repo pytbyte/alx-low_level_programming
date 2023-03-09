8f92d88ee3fb03d22f05a0ed47681d3848b1d21a
pytbyte <heretolearn1@gmail.com>
2023-03-09 03:58:55 +0300

    Recursion manipulations with c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int _sqrt_recursion(int n, int i);
+
+/**
+ * _sqrt_recursion - a function that returns the square root of a number
+ * @n: number to  check sq root
+ *
+ * Return: square root obtained
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (_sqrt_recursion(n, 0));
+}
+
+/**
+ * _sqrt_recursion - This recurses to find the square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+int _sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_sqrt_recursion(n, i + 1));
+}
