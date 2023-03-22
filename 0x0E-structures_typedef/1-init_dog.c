d0a505237615f6647245a05b9032cff4e0abaf83
pytbyte <heretolearn1@gmail.com>
2023-03-22 12:16:37 +0300

    correction for struc dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,31 +1,18 @@
-#ifndef DOG_H
-#define DOG_H
+#include <stdlib.h>
+#include "dog.h"
 
 /**
- * struct dog - my dog's profile
- * @name: dogs name
- * @age: dogs age
- * @owner: me ;)
- *
- * Description: detailed description
+ * init_dog - variable type struct dog
+ * @d: pointer to struct dog
+ * @name: name of dog
+ * @age: age to dog
+ * @owner: owner of dog
  */
-struct dog
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	char *name;
-	float age;
-	char *owner;
-};
-
-/**
- * dog_t - typedef for struct dog
- */
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
-
-#endif
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
