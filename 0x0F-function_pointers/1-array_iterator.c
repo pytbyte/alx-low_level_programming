65dc0bfdb15bd00d573218666e56e78aca0f77b6
pytbyte <heretolearn1@gmail.com>
2023-03-23 14:36:16 +0300

    task 1 function pointers array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a parameter
+ * on each element of an array
+ * @array: array 
+ * @size: size of the array
+ * @action: pointer to function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int x;
+
+	if (!array || !action)
+		return;
+
+	for (x = 0; x < size; x++)
+		action(array[x]);
+}
