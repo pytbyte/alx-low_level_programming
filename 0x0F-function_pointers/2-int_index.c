ca528e14ac017191fec9b0fd4ceea2366528bc01
pytbyte <heretolearn1@gmail.com>
2023-03-23 14:41:25 +0300

    task 1 function pointers array search

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for intr
+ * @array: array to interogate
+ * @size: size of the array
+ * @cmp: pointer to the comparing function
+ *
+ * Return: index of the first element for which
+ * the cmp function does not return 0, or -1 if no match is found
+ * or size is negative
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x;
+
+	if (array && cmp)
+	{
+		for (x = 0; x < size; x++)
+		{
+			if (cmp(array[x]) != 0)
+				return (x);
+		}
+	}
+
+	return (-1);
+}
