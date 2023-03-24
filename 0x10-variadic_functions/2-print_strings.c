20e7f1e88a2084df61671dbcc664b85ca513a068
pytbyte <heretolearn1@gmail.com>
2023-03-24 11:32:24 +0300

    2. One woman's constant is another woman's variable #correctin3

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,17 +15,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(listed, n);
 
+	str =va_arg(listed, char *);
 	for (i = 0; i < n; i++)
 	{
-	if (!str)
+		if (!str)
 
-		str = "(nill)";
-	if (!separator)
-		printf("%s", str);
-	else if (separator && i == 0)
-		printf("%s", str);
-	else
-		printf("%s%s", separator, str);
+			str = "(nill)";
+		if (!separator)
+			printf("%s", str);
+		else if (separator && i == 0)
+			printf("%s", str);
+		else
+			printf("%s%s", separator, str);
 	}
 	printf("\n");
 	va_end(listed);
