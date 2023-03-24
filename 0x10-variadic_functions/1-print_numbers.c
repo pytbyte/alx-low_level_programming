0aa1ec39e5885396be4ad4ea8e701f97d3bb033d
pytbyte <heretolearn1@gmail.com>
2023-03-24 09:39:05 +0300

    1. To be is to be the value of a variable #correction 3

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,28 +1,30 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - prints numbers given as parameters
- * @separator: string to be printed between numbers
- * @n: number of integers passed to the function
- */
+* print_numbers -a function that prints numbers, followed by a new line
+*
+* Description - a function that prints numbers, followed by a new line
+* @separator: the string to be printed between numbers
+* @n: number of integers passed to the function
+* Return  Null
+*/
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+
 	unsigned int i;
-	va_list list;
+	va_list listed;
 
-	va_start(list, n);
-
+	va_start(listed, n);
 	for (i = 0; i < n; i++)
 	{
 		if (!separator)
-			printf("%d", va_arg(list, int));
-		else if (separator && i == 0)
-			printf("%d", va_arg(list, int));
+			printf("%d", va_arg(listed, int));
+		else if (separator && n == 0)
+			printf("%d", va_arg(listed, int));
 		else
-			printf("%s%d", separator, va_arg(list, int));
+			printf("%s%d", separator, va_arg(listed, int));
 	}
-
-	va_end(list);
-
 	printf("\n");
+	va_end(listed);
 }
