ba28970e7773705de49d56238fb68191565f9213
pytbyte <heretolearn1@gmail.com>
2023-03-24 13:32:41 +0300

    3. To be is a to be the value of a variable

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	unsigned int i = 0, j;
+	char *str;
+	const char *sep = "";
+
+	va_start(args, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (g_format[j].type)
+		{
+	if (*(g_format[j].type) == format[i])
+				{
+					printf("%s", sep);
+					str = g_format[j].f(args);
+					printf("%s", str);
+					sep = ", ";
+					free(str);
+					break;
+				}
+				j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
