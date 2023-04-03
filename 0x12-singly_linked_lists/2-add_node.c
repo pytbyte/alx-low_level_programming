54227fdede41a4bb0274da74e400de6682262faa
pytbyte <heretolearn1@gmail.com>
2023-04-03 14:43:21 +0300

    Singly linked Lists add node 02 -edit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,17 +12,17 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	unsigned int count = 0;
+	unsigned int len = 0;
 
-	while (str[count])
-		count++;
+	while (str[len])
+		len++;
 
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->count = count;
+	new->len = len;
 	new->next = (*head);
 	(*head) = new;
 
