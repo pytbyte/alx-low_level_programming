9632e72f22a4a06bb23295248dd3d92cd03bc2cf
pytbyte <heretolearn1@gmail.com>
2023-05-01 21:26:50 +0300

    Task 9. Insert node at some position

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: pointer to the first node in the list
+ * @idx: index where the new node is added
+ * @n: data to insert in the new node
+ *
+ * Return: pointer to the new node, or NULL
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int count;
+	listint_t *new_node;
+	listint_t *temp_head = *head;
+
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node || !head)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	for (count = 0; temp_head && count < idx; count++)
+	{
+		if (count == idx - 1)
+		{
+			new_node->next = temp_head->next;
+			temp_head->next = new_node;
+			return (new_node);
+		}
+		else
+			temp_head = temp_head->next;
+	}
+
+	return (NULL);
+}
