30baeddcafe42709644ad23de87f110d72e560cd
pytbyte <heretolearn1@gmail.com>
2023-05-01 20:01:20 +0300

    Task 5. Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer to the listint_t list to be freed
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp_head;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		temp_head = (*head)->next;
+		free(*head);
+		*head = temp_head;
+	}
+
+	*head = NULL;
+}
