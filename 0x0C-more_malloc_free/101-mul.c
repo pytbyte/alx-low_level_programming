26eae0b620f2ff451c32d4a575beb5725cd7756a
pytbyte <heretolearn1@gmail.com>
2023-05-03 13:49:44 +0300

    5. We must accept finite disappointment, but never lose infinite hope -edited 2

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,39 +3,39 @@
 #include "main.h"
 
 /**
- * is_digit - checks if a string contains digits only.  
- * @s: string to be checked
+ * is_digit - checks for non-digit char in string
+ * @s: string to check
  *
- * Return: 0 if string doest contain digits only , 1 otherwise
+ * Return: 0 or 1 
  */
 int is_digit(char *s)
 {
-	int t = 0;
+	int i = 0;
 
-	while (s[t])
+	while (s[i])
 	{
-		if (s[t] < '0' || s[t] > '9')
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
-		t++;
+		i++;
 	}
 	return (1);
 }
 
 /**
  * _strlen - returns the length of a string
- * @s: string to check length
+ * @s: string to evaluate
  *
  * Return: the length of the string
  */
 int _strlen(char *s)
 {
-	int len_t = 0;
+	int i = 0;
 
-	while (s[len_t] != '\0')
+	while (s[i] != '\0')
 	{
-		len_t++;
+		i++;
 	}
-	return (len_t);
+	return (i);
 }
 
 /**
@@ -57,32 +57,32 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len01, len02, len, i, carry, digit1, digit2, *result, a = 0;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-	len01 = _strlen(s1);
-	len02 = _strlen(s2);
-	len = len01 + len02 + 1;
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	for (i = 0; i <= len01 + len02; i++)
+	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-	for (len01 = len01 - 1; len-1 >= 0; len01--)
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		digit1 = s1[len01] - '0';
+		digit1 = s1[len1] - '0';
 		carry = 0;
-		for (len02 = _strlen(s2) - 1; len02 >= 0; len02--)
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			digit2 = s2[len02] - '0';
-			carry += result[len01 + len02 + 1] + (digit1 * digit2);
-			result[len01 + len02 + 1] = carry % 10;
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
 		if (carry > 0)
-			result[len01 + len02 + 1] += carry;
+			result[len1 + len2 + 1] += carry;
 	}
 	for (i = 0; i < len - 1; i++)
 	{
