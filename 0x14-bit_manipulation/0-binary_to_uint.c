6bfe0a9fb585edbcbfb4eec5718109030a141606
pytbyte <heretolearn1@gmail.com>
2023-05-04 23:03:25 +0300

    0 - bits manipulated -edit3

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 
 /**
  * binary_to_uint - convert a binary number to an unsigned int
- * @b: char 
+ * @b: char
  * Return: converted decimal number or 0 if there is an unconvertable char
  */
 unsigned int binary_to_uint(const char *b)
