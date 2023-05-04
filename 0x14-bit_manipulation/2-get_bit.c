a996412ed3611dd8745164d4f8af59a51e89f079
pytbyte <heretolearn1@gmail.com>
2023-05-05 00:07:39 +0300

    get bit edited

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,7 +5,7 @@
  * get_bit - get the value of a bit at a given index
  * @n: number to evaluate
  * @index: index starting from 0, of the bit we want to get
- * Return: Value of bit at index, or -1 if error
+ * Return: Value of bit at index, or -1 if error occured
  */
 
 int get_bit(unsigned long int n, unsigned int index)
