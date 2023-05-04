5b7f2153f803722bbd19aa8d457da1397ba8e354
pytbyte <heretolearn1@gmail.com>
2023-05-05 00:25:53 +0300

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * flip_bits - flip bits to convert one number to another number
+ * @n: first number
+ * @m: second number
+ * Return: number of bits that was needed to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int temp_store;
+	int counter;
+
+	temp_store = n ^ m;
+	counter = 0;
+
+	while (temp_store)
+	{
+		counter++;
+		temp_store &= (temp_store - 1);
+	}
+
+	return (counter);
+}
