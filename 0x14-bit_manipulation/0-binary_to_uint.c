c4549072711d4d5f551e91a913ee696517a67f54
pytbyte <heretolearn1@gmail.com>
2023-05-04 22:42:22 +0300

    0 - bits manipulated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,30 +1,28 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: pointer to a string of 0 and 1 chars
+ * binary_to_uint -converts a binary number to an unsigned int.
+ * @b: pointing to a string of 0 and 1 chars
  *
- * Return: the converted number, or 0 if there is one or more chars
- * in the string b that is not 0 or 1 or b is NULL
+ * Return:converted number, or 0
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int unit_storage = 0;
+	int count;
+	unsigned int unit_storage;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
-
-	for (; *b != '\0'; b++)
+	for (count = 0; b[count] != '\0'; count++)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[count] != '0' && b[count] != '1')
 			return (0);
-
+	}
+	for (i = 0; b[count] != '\0'; count++)
+	{
 		unit_storage <<= 1;
-
-		if (*b == '1')
+		if (b[count] == '1')
 			unit_storage += 1;
 	}
-
 	return (unit_storage);
 }
