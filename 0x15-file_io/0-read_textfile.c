e0bd3a5bbdf592f96027c30cdb5a516f87f7aa9d
pytbyte <heretolearn1@gmail.com>
2023-05-08 21:18:41 +0300

    file I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buffer == NULL)
 		return (0);
 
-	o = open(filename, O_RDONLY);
+	o = open(STDIN_FILENO, filename, O_RDONLY);
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 
