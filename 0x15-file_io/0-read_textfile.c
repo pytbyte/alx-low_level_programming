b589d540b569708c2705613fb1db8b64fe805ca2
pytbyte <heretolearn1@gmail.com>
2023-05-08 20:59:55 +0300

    file I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,25 +11,24 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	/* Check if filename is NULL */
 	if (filename == NULL)
 		return (0);
 
-	/* Open the file for reading */
 	FILE *fp = fopen(filename, "r");
+
 	if (fp == NULL)
 		return (0);
 
-	/* Allocate a buffer to hold the file contents */
 	char *buffer = malloc(sizeof(char) * (letters + 1));
+
 	if (buffer == NULL)
 	{
 		fclose(fp);
 		return (0);
 	}
 
-	/* Read up to 'letters' characters from the file into the buffer */
 	ssize_t nread = fread(buffer, sizeof(char), letters, fp);
+
 	if (ferror(fp) || nread == 0)
 	{
 		free(buffer);
@@ -37,11 +36,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	/* Add a null terminator to the buffer */
 	buffer[nread] = '\0';
 
-	/* Write the buffer to stdout */
 	ssize_t nwritten = fwrite(buffer, sizeof(char), nread, stdout);
+
 	if (nwritten < nread)
 	{
 		free(buffer);
@@ -49,10 +47,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	/* Free the buffer and close the file */
 	free(buffer);
 	fclose(fp);
 
-	/* Return the number of characters read and written to stdout */
 	return (nread);
 }
+
