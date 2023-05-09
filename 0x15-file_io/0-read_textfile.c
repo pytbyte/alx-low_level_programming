50aebe72d2291aa50f6f4c58aff5070ebe3977bf
pytbyte <heretolearn1@gmail.com>
2023-05-09 11:42:56 +0300

    Betty issues fixed 2

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,26 +13,26 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, r, w;
-	char *buffer;
-
+	char *buff;
+	
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
 		return (0);
 
 	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
+	r = read(o, buff, letters);
+	w = write(STDOUT_FILENO, buff, r);
 
 	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		free(buffer);
+		free(buff);
 		return (0);
 	}
 
-	free(buffer);
+	free(buff);
 	close(o);
 
 	return (w);
