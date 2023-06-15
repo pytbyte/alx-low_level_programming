d68abb8b8d6283f79a31b9ddf91d739b1ac55d86
pytbyte <heretolearn1@gmail.com>
2023-06-15 12:45:19 +0300

    prints all the elements of a dlistint_t list.

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * print_dlistint - Prints all the elements of a doubly linked list
+ * @h: Pointer to the head of the list
+ *
+ * Return: The number of nodes in the list
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t node_count = 0;
-    const dlistint_t *current = h;
+	size_t node_count = 0;
+	const dlistint_t *current = h;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-        node_count++;
-    }
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		node_count++;
+	}
 
-    printf("Total nodes: %lu\n", node_count);
+	printf("Total nodes: %lu\n", node_count);
 
-    return node_count;
+	return (node_count);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -3,12 +3,19 @@
 
 #include <stddef.h>
 
-/* Structure for doubly linked list */
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 /* Function prototypes */
