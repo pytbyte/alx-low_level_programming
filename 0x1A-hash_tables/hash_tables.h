ce15f30306f5dffcd370ac2835653aa67bd16d1b
pytbyte <heretolearn1@gmail.com>
2023-07-07 11:49:28 +0300

    fixed string import

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,5 +1,9 @@
-#ifndef hash_tbl
-#define hash_tbl
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -11,9 +15,9 @@
  */
 typedef struct hash_node_s
 {
-char *key;
-char *value;
-struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -26,9 +30,16 @@ struct hash_node_s *next;
  */
 typedef struct hash_table_s
 {
-unsigned long int size;
-hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+
 #endif
