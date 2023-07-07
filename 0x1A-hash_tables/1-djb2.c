f60fadf6a261ac313100efa68d6f96635d2430fe
pytbyte <heretolearn1@gmail.com>
2023-07-07 11:47:03 +0300

    hash function implementing the djb2 algorithm

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,6 +1,3 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "hash_tables.h"
 /**
  * hash_djb2 - implementation of the djb2 algorithm
@@ -14,7 +11,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 	int c;
 
 	hash = 5381;
-	while ((c = *str++))
+    while ((c = *str++))
 	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
