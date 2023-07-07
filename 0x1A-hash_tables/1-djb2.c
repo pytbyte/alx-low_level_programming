7ff918060bfb739b3813623a6c47ea08aefb254c
pytbyte <heretolearn1@gmail.com>
2023-07-07 11:38:01 +0300

    hash function implementing the djb2 algorithm

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,7 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
 /**
  * hash_djb2 - implementation of the djb2 algorithm
  * @str: string used to generate hash value
