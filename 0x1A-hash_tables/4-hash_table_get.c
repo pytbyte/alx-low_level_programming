7e1b95bec9b96d4c2d12ddedc607e94906b43c1b
pytbyte <heretolearn1@gmail.com>
2023-07-07 18:13:29 +0300

    a function that retrieves a value associated with a key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,6 +1,8 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_get - Retrieves the value associated with a given key from a hash table.
+ * hash_table_get - Retrieves the value associated
+ * with a given key from a hash table.
  * @ht: The hash table to search in.
  * @key: The key to search for.
  *
@@ -8,22 +10,22 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
-    hash_node_t *temp;
+	unsigned long int index;
+	hash_node_t *temp;
 
-    if (ht == NULL || key == NULL || *key == '\0')
-        return NULL;
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
 
-    index = key_index((const unsigned char *)key, ht->size);
-    temp = ht->array[index];
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
 
-    while (temp != NULL)
-    {
-        if (strcmp(temp->key, key) == 0)
-            return temp->value;
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
 
-        temp = temp->next;
-    }
+		temp = temp->next;
+	}
 
-    return NULL;
+	return (NULL);
 }
