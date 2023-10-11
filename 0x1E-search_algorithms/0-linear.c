c1ccafd0defffa8de30ecb93175bdf5cfffd6b0e
pytbyte <heretolearn1@gmail.com>
2023-10-11 15:18:08 +0300

    search algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,16 +1,15 @@
 #include "search_algos.h"
 
 /**
- * * 0-linear - searches for a value in an array of 
- *              integers using the Linear search algorithm
- * @array:  a pointer to the first element of the array to search in
- * @size:  the number of elements in array
+ * linear_search - searches for a value in an array of integers using
+ *                 the Linear search algorithm
+ * @array: a pointer to the first element of the array to search in
+ * @size: the number of elements in array
  * @value: the value to search for
  *
- * Return:  first index where value is located
+ * Return: the first index where value is located, or -1 if not found
  */
-
-int linear_search(int *array, size_t size, int value){
+int linear_search(int *array, size_t size, int value) {
     if (array == NULL) {
         return -1;
     }
@@ -22,5 +21,4 @@ int linear_search(int *array, size_t size, int value){
         }
     }
     return -1;
-
 }
