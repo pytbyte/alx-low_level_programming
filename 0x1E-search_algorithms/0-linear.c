d62aecc1fab9d3198fdf44bfdd44a1f284705041
pytbyte <heretolearn1@gmail.com>
2023-10-11 15:50:59 +0300

    search algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,14 +10,15 @@
  * Return: the first index where value is located, or -1 if not found
  */
 int linear_search(int *array, size_t size, int value)
-{   size_t i;
+{
+	size_t i;
 
 	if (array == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%lu] = %d\n", i, array[i]);
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
